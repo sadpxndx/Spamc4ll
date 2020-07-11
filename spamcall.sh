3c41fb4944bb291e21b05085e312d77fde12b71d
rootpandax <52450467+rootpandax@users.noreply.github.com>
2020-07-12 01:43:02 +0700

    Update spamcall.sh

--- spamcall.sh
+++ spamcall.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 clear
-figlet PANDA | lolcat 
-echo "
+figlet PANDA CALLER | lolcat
+echo 'made by panda, Instagram = mfakhri_f'
+echo'
 [1].Jalankan scriptnya
-[2].keluar
-"
+[2].keluar.
+'
 read -p "lu mau yang mana : " pil
 if [[ $pil == 1 ]]; then
-no "Masukkan no tujuan : " nomor
+read -p "Masukkan no tujuan : " nomor
 link="https://id.jagreward.com/member/verify-mobile/$nomor"
 curl -s $link
 else
 echo 'SOK ATUH KELUAR ANJING'
+echo 'bye'
 exit
 fi
-;
 echo ''

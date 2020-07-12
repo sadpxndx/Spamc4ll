df2ba5f9f05398af1c83c28276aef9e1a283a61b
rootpandax <52450467+rootpandax@users.noreply.github.com>
2020-07-13 04:49:31 +0700

    Update spamcall.sh

--- spamcall.sh
+++ spamcall.sh
@@ -6,8 +6,8 @@ echo '===================================='
 echo 'made by panda, Instagram = mfakhri_f'
 echo '===================================='
 echo '
-[1].Jalankan scriptnya
-[2].keluar
+[1].run the script
+[2].exit
 '
 echo
 read -p "lu mau yang mana : " pil
@@ -16,7 +16,7 @@ read -p "Masukkan no tujuan : " nomor
 link="https://id.jagreward.com/member/verify-mobile/$nomor"
 curl -s $link
 else
-echo 'SOK ATUH KELUAR ANJING'
+echo 'ok see you next time'
 echo 'bye'
 exit
 fi
